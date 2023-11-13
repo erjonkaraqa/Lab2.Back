c448c60c6c6b615140189db3c9e3606708e72f6c
bledonibishi <bledonibishi1@gmail.com>
2023-11-13 01:11:50 +0100

    product routes authenticated

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const productController = require("../controllers/productController");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
 router.get("/", productController.getAllProducts);
 router.get("/categories", productController.getAllProductCategories);
 router.get("/:id", productController.getProduct);
-router.post("/createProductCategory", productController.createProductCategory);
+router.post(
+  "/createProductCategory",
+  authController.protect,
+  authController.restrictTo("admin"),
+  productController.createProductCategory
+);
 router.post(
   "/createProduct",
   productController.uploadProductImages,
   productController.resizeProductImages,
+  authController.protect,
+  authController.restrictTo("admin"),
   productController.createProduct
 );
 router.patch(
   "/:id",
   productController.uploadProductImages,
   productController.resizeProductImages,
+  authController.protect,
+  authController.restrictTo("admin"),
   productController.updateProduct
 );
 
